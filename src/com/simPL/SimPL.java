58f6451e636806b35b3aa18a59bc8a7cbe83b3fc
xyz <xyzhu1120@gmail.com>
2013-05-28 15:25:03 +0800

    finish a tentative work of syntax tree

--- src/com/simPL/SimPL.java
+++ src/com/simPL/SimPL.java
@@ -1,5 +1,8 @@
 package com.simPL;
 
+import com.simPL.compiler.SIMPL;
+import com.simPL.compiler.SimpleNode;
+
 public class SimPL {
 
 	/**
@@ -7,7 +10,20 @@ public class SimPL {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	    System.out.println("Reading from standard input...");
+	    System.out.print("Enter an expression like \"1+(2+3)*var;\" :");
+	    new SIMPL(System.in);
+	    try
+	    {
+	      SimpleNode n = SIMPL.Start();
+	      n.dump("");
+	      System.out.println("Thank you.");
+	    }
+	    catch (Exception e)
+	    {
+	      System.out.println("Oops.");
+	      System.out.println(e.getMessage());
+	    }
 	}
 
 }
